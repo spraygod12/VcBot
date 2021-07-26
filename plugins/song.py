0e66d0c4d6a0e136beca988063b26993e24a42cc
∂ιρєѕн/Bᴏᴛ <83916289+Itzdipesh@users.noreply.github.com>
2021-07-26 19:33:00 +0530

    Update song.py

--- plugins/song.py
+++ plugins/song.py
@@ -42,7 +42,7 @@ def a(client, message):
             #     m.edit("Exceeded 30mins cap")
             #     return
 
-            performer = f"[†hê Hêllẞø†]" 
+            performer = f"Pᴀɴᴅɪᴛ ᴍᴜsɪᴄ ʙᴏᴛ❥" 
             thumb_name = f'thumb{message.message_id}.jpg'
             thumb = requests.get(thumbnail, allow_redirects=True)
             open(thumb_name, 'wb').write(thumb.content)
